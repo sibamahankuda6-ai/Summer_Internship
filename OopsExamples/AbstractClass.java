38a64d7ba5f9bc812339f75e5dbe030f9d547a1a
Siba Mahankuda <sibamahankuda6@gmail.com>
2026-05-25 20:57:27 +0530

    Abstraction

--- OopsExamples/AbstractClass.java
+++ OopsExamples/AbstractClass.java
@@ -1,4 +1,20 @@
 package Summer_Internship.OopsExamples;
 
-public class AbstractClass {
+public  abstract class AbstractClass {
+    abstract void hello();
+    public void hii(){
+        System.out.println("hello guys");
+    }
+
+}
+class Child extends AbstractClass{
+    void hello(){
+        System.out.println(" har har mahadev");
+    }
+
+    static void main(String[] args) {
+        Child c = new Child();
+        c.hii();
+        c.hello();
+    }
 }
