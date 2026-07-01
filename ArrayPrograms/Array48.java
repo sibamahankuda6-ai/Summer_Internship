453af443468dc918e8b8dd4ad744916335934831
Siba Mahankuda <sibamahankuda6@gmail.com>
2026-07-01 23:08:33 +0530

    sort array
    ascending order

---
+++ ArrayPrograms/Array48.java
@@ -0,0 +1,44 @@
+//Write a Java program to sort array elements in ascending order
+//Input-{9,5,6,1,2,7}
+//Output-{1,2,5,6,7,9}
+package Summer_Internship.ArrayPrograms;
+import java.util.*;
+public class Array48 {
+
+
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sort(arr);
+
+        System.out.print("Sorted array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
+
