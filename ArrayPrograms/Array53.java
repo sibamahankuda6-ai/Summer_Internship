f4c3a7ef716fa6fa47fe0bf16bbb543fff4715d2
Siba Mahankuda <sibamahankuda6@gmail.com>
2026-07-02 23:37:30 +0530

    quick sort
    quick sort

---
+++ ArrayPrograms/Array53.java
@@ -0,0 +1,54 @@
+//Write a Java program for quick sorting.
+package Summer_Internship.ArrayPrograms;
+import java.util.*;
+public class Array53 {
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted Array:");
+        for (int num : arr)
+            System.out.print(num + " ");
+
+        sc.close();
+    }
+}
