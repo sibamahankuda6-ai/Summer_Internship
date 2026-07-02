02396b95bba78703a5ceea0e97b9b7fd7d50c512
Siba Mahankuda <sibamahankuda6@gmail.com>
2026-07-02 23:35:47 +0530

    insertion sort
    insertion sort

---
+++ ArrayPrograms/Array51.java
@@ -0,0 +1,39 @@
+//Write a Java program for insertion sort.
+package Summer_Internship.ArrayPrograms;
+import java.util.*;
+public class Array51 {
+    static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        insertionSort(arr);
+
+        System.out.println("Sorted Array:");
+        for (int num : arr)
+            System.out.print(num + " ");
+
+        sc.close();
+    }
+}
